34a666ebe72bbd7ecd8dd94d4c13eb41477b568e
Metamist <metamist@ymail.com>
2015-01-02 02:16:51 +0100

    Merge branch 'master' of https://github.com/DuckyC/devinity2

--- plugins/bot.lua
+++ plugins/bot.lua
@@ -2,6 +2,7 @@
 
 local actions = {"Warping to Station", "Warping to Asteroid", "Selling", "Mining"}
 local activeConVar = CreateClientConVar( "dv2_automine", "0" )
+local ConVarStatus = CreateClientConVar( "dv2_automine_status", "0" )
 local lp = LocalPlayer()
 local action = 0
 
@@ -12,23 +13,35 @@ local function Mine()
 	for k, v in pairs( lp.Equipment ) do ToggleFire( k, true ) end
 end
 
+local function SA(na)
+	if ConVarStatus:GetInt() == 1 and action != na then 
+		print("DV2 Bot:", actions[na]) 
+		if na == 2 then
+			print("DV2 Bot: ", string.Comma(lp:GetMoney()).." GCS")
+		end
+	end
+	action = na
+end
+
 local w,h = ScrW(), ScrH()
 hook.Add("HUDPaint", "action", function()
 	if activeConVar:GetInt() == 0 then return end
 	DrawText(actions[action] or "no action", "DefaultSmall", w/2, h/2, MAIN_TEXTCOLOR)
 end)
 
+local Run = RunConsoleCommand
 local lastrun = 0
 hook.Add("Tick", "dv2_automine_tick", function() 
 	if activeConVar:GetInt() == 0 then return end
 	if CurTime() < lastrun + 1 then return end
 	//Warping
-	if not DV2P.IsMoving() and DV2P.IsInventoryFull() and (not DV2P.IsAt("Station")) then DV2P.WarpToNearest("Station") action = 1 end
-	if not DV2P.IsMoving() and not DV2P.IsInventoryFull() and (not DV2P.IsAt("AsteroidField")) then DV2P.WarpToNearest("AsteroidField") action = 2 end
+	if not DV2P.IsMoving() and DV2P.IsInventoryFull() and (not DV2P.IsAt("Station")) then DV2P.WarpToNearest("Station") SA(1) end
+	if not DV2P.IsMoving() and not DV2P.IsInventoryFull() and (not DV2P.IsAt("AsteroidField")) then DV2P.WarpToNearest("AsteroidField") Run("-forward") SA(2) end
 
 	//Actions
-	if not DV2P.IsMoving() and DV2P.IsInventoryFull() and DV2P.IsAt("Station") then DV2P.SellEverything() action = 3 end
-	if not DV2P.IsMoving() and not DV2P.IsInventoryFull() and DV2P.IsAt("AsteroidField") then Mine() action = 4 end
+	if DV2P.IsInventoryFull() and DV2P.IsAt("Station") and not DV2P.CanDock(true) then Run("+forward") end
+	if not DV2P.IsMoving() and DV2P.IsInventoryFull() and DV2P.IsAt("Station") then DV2P.SellEverything() SA(3) end
+	if not DV2P.IsMoving() and not DV2P.IsInventoryFull() and DV2P.IsAt("AsteroidField") then Mine() SA(4) end
 
 	if lp:GetDocked() and not DV2P.IsInventoryFull() then lp:RequestUndock() StationMenu:SetVisible( false ) end
 
--- plugins/findbest.lua
+++ plugins/findbest.lua
@@ -1,7 +1,7 @@
 concommand.Add("dv2_bestore", function()
 	local ores = {}
 	for ID, System in pairs(GAMEMODE.SolarSystems) do
-		ores[ID] = {ID=ID, Ore = GenerateItem(ID,System.Tech,"Resource","Any",true)}
+		ores[ID] = {ID=ID, Ore = GenerateItem(ID,System.Tech+100,"Resource","Any",true)}
 	end
 	table.sort(ores, function(a,b) return a.Ore.Price > b.Ore.Price end)
 	PrintTable(ores[1])
@@ -9,9 +9,27 @@ concommand.Add("dv2_bestore", function()
 end)
 
 concommand.Add("dv2_oreinfo", function()
-	local Ores = DV2p.GenerateAllMiningOres(GAMEMODE.SolarSystems)
+	local Ores = DV2P.GenerateAllMiningOres(GAMEMODE.SolarSystems)
 	print("ID,Tech,Ore Name,Base Price,System Name")
 	for k,v in pairs(Ores) do
 		print(k..","..v.Tech..","..v.Name..","..v.Price..","..GAMEMODE.SolarSystems[k].Name)
 	end
+end)
+
+concommand.Add("dv2_vcraft", function(_,_,ores)
+	local pl = LocalPlayer()
+	local crafting = pl:GetSkillLevel("Crafting")^2
+	local mining = pl:GetSkillLevel("Mining")^2
+	local CraftingList = {}
+	for k,v in pairs(ores) do 
+		local ID = tonumber(v) 
+		if ID then 
+			local System = GAMEMODE.SolarSystems[ID]
+			local Data = GenerateItem(ID,System.Tech+mining,"Resource","Any",true)
+			if Data then CraftingList[#CraftingList+1] = Data else return end
+		else return end
+	end
+	local Data = CraftItem(CraftingList,crafting)
+	print(Data.Type.." "..Data.Class.." "..Data.Rarity.Name)
+	print("Tech: "..Data.Tech,"DMG: "..Data.Dmg, "CD: "..Data.CD, "DPS: "..(Data.Dmg / Data.CD))
 end)
