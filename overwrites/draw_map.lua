a6e52b1090035119b0ab22605b00cd9cad1d3d17
Metamist <metamist@ymail.com>
2015-01-02 14:46:40 +0100

    Added Pre_MAP_Frame_Paint and Post_MAP_Frame_Paint

--- overwrites/draw_map.lua
+++ overwrites/draw_map.lua
@@ -4,5 +4,17 @@ DV2P.Overrides.OpenMap = DV2P.Overrides.OpenMap or OpenMap
 function OpenMap()
 	DV2P.OFF.RunFunction( "Pre_OpenMap" )
 	DV2P.Overrides.OpenMap()
+
+	if IsValid( MAP_Frame ) then
+		DV2P.Overrides.MAP_Frame_Paint = DV2P.Overrides.MAP_Frame_Paint or MAP_Frame.Paint
+
+		print( "override" )
+		function MAP_Frame:Paint( w, h )
+			DV2P.OFF.RunFunction( "Pre_MAP_Frame_Paint", self, w, h )
+			DV2P.Overrides.MAP_Frame_Paint( self, w, h )
+			DV2P.OFF.RunFunction( "Post_MAP_Frame_Paint", self, w, h )
+		end
+	end
+
 	DV2P.OFF.RunFunction( "Post_OpenMap" )
 end
