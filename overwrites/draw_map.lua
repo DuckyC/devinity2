93383d2182376fc56732e98a75ec2f062f2dd6e9
Metamist <metamist@ymail.com>
2015-01-04 22:52:13 +0100

    Fixed Map local scope is wrong when reloading

--- overwrites/draw_map.lua
+++ overwrites/draw_map.lua
@@ -114,6 +114,16 @@ function DV2P.Map.ToScreen( vec )
 	}
 end
 
+local DermaMapLocalButtonDoClick = function(s) 
+	local lp = LocalPlayer()
+	if (!lp.PlayerPos or !lp.FloatPos) then return end
+	
+	LocalMap = !LocalMap 
+	
+	if (LocalMap) then Zoom = math.Clamp(Zoom,2,70) Origin = Zero*1 s.Text = "Exit Local Map"
+	else Zoom = Zoom*10 s.Text = "Enter Local Map" end
+end
+
 function MouseWheelZoom_map(s,delta)
 	if (!IsMapOpen()) then return end
 	if (LocalMap) then Zoom = math.Clamp(Zoom-delta*50,20,7000)
@@ -246,244 +256,6 @@ function OpenMap()
 			Col.a = A
 		end
 		
-		MAP_Frame.Paint = function(s,w,h)
-			DV2P.OFF.RunFunction( "Pre_MAP_Frame_Paint", s, w, h )
-			local lp = LocalPlayer()
-			
-			if (!lp.PlayerPos or !lp.FloatPos) then return end
-			
-			DrawRect(0,0,w,h,Blac)
-			
-			local Hover 		= 0
-			local HoverEntity 	= 0
-			local Reg,ID = lp:GetRegion()
-			
-			local MouseInList = input.IsMouseInBox(SysListDim.x,SysListDim.y,SysListDim.w,SysListDim.h)
-			
-			local FactControls = {}
-			
-			local OriginSPos
-			
-			cam.Start(CamData)
-				DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D", CamData )
-
-				if (LocalMap and Reg != "Space") then
-					local Scale = MAIN_SOLARSYSTEM_RADIUS/100
-					DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_LocalMap", CamData, Scale )
-					
-					for k,v in pairs(GAMEMODE.MapEnts) do v.SPos = ((v.Pos-GAMEMODE.SolarSystems[ID].Pos)/Scale+v.FloatPos/Scale):ToScreen() end
-					
-					lp.SPos = ((lp.PlayerPos-GAMEMODE.SolarSystems[ID].Pos)/Scale+lp.FloatPos/Scale):ToScreen()
-					
-					if (lp.WarpDest) then
-						lp.WarpSPos = ((lp.WarpDest-GAMEMODE.SolarSystems[ID].Pos)/Scale+lp.WarpDestDetail/Scale):ToScreen()
-					end
-					
-					RSetMaterial(Prox)
-					DrawQuadEasy(Zero,Up,200,200,MAIN_COLOR,CurTime()/3)
-
-					DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_LocalMap", CamData, Scale )
-				else
-					DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_SystemMap", CamData, Scale )
-					local Scale = MAIN_MAP_SIZE/2500
-					local Data	= lp:GetShipData()
-					local Radius = Data.MaxWarpDistance/Scale
-					local LPRealPos = lp.PlayerPos/Scale+lp.FloatPos/Scale
-					local AimFace	= -CamData.angles:Forward()
-					
-					RSetMaterial(Clouds)
-					for k,v in pairs(s.Clouds) do DrawQuadEasy(v.Pos,AimFace,v.Size,v.Size,v.Col,v.Yaw) end
-					
-					RSetMaterial(matLight)
-					
-					for k,v in pairs(GAMEMODE.SolarSystems) do 
-						DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_System", CamData, v, Scale )
-						local SunRealPos = v.Pos/Scale
-						v.SPos = SunRealPos:ToScreen() 
-						--DrawQuadEasy(SunRealPos,AimFace,50,50,v.Color,0)
-						DrawQuadEasy(SunRealPos,AimFace,5,5,v.TechColor,0)
-						
-						if (input.IsMouseInBox(v.SPos.x-14,v.SPos.y-14,28,28) and Reg!=v.Name and !MouseInList) then Hover = k end
-						
-						if (v.Owner) then
-							if (!FactControls[v.Owner]) then FactControls[v.Owner] = {} end
-							insert(FactControls[v.Owner],v.Pos)
-							
-							local Col = GetFactionColor(v.Owner)
-							Col.a = 20
-							
-							RSetMaterial(Bare)
-							render.DrawSphere( SunRealPos, 30, 32, 32, Col )
-							RSetMaterial(matLight)
-						end
-						
-						if (v.Connected) then
-							RSetMaterial(Bare)
-							for k2,CPos in pairs(v.Connected) do
-								RDrawBeam( SunRealPos, CPos/Scale, 1, 0,1,LineC )
-							end
-							RSetMaterial(matLight)
-						end
-						DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_System", CamData, v, Scale )
-					end 
-					
-					for faction,Controls in pairs(FactControls) do
-						local Sum = Zero
-						
-						for i,pos in pairs(Controls) do
-							Sum = Sum+pos
-						end
-						
-						local Avg = Sum/#Controls
-						
-						FactControls[faction] = (Avg/Scale):ToScreen()
-					end
-					
-					lp.SPos = LPRealPos:ToScreen()
-					
-					if (lp.WarpDest) then
-						lp.WarpSPos = (lp.WarpDest/Scale+lp.WarpDestDetail/Scale):ToScreen()
-					end
-					
-					RSetMaterial(Bare)
-					render.DrawSphere( LPRealPos, Radius, 32, 32, WarpCol )
-					render.DrawWireframeSphere( LPRealPos, Radius, 32, 32, WarpCol )
-					
-					OriginSPos = Zero:ToScreen()
-					DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_SystemMap", CamData, Scale )
-				end
-				DV2P.OFF.RunFunction( "Post_OpenMap_Render3D", CamData )
-			cam.End()
-			
-			if (LocalMap and Reg != "Space") then
-				for k,v in pairs(GAMEMODE.MapEnts) do
-					if (table.HasValue(DisplayTable,v.Class)) then
-						DrawOutlinedRect(v.SPos.x-1,v.SPos.y-1,2,2,MAIN_WHITECOLOR)
-						DrawText(v.Class.." "..k,"DVTextSmall",v.SPos.x,v.SPos.y-20,MAIN_TEXTCOLOR,1)
-						if (input.IsMouseInBox(v.SPos.x-14,v.SPos.y-14,28,28) and (v.Pos-lp.PlayerPos-lp.FloatPos):Length() > 5000) then HoverEntity = k Hover = ID end
-					end
-				end
-				
-				DrawText(GAMEMODE.SolarSystems[ID].Name,"DVText",ScrW()/2,80,MAIN_TEXTCOLOR,1)
-			else
-				--System List
-				local FT = 0.001*(UnPredictedCurTime()-FT)
-				local C = (1+math.cos(CurTime()*3))/2
-				local Scale = MAIN_MAP_SIZE/2500
-				
-				for faction,Pops in pairs(FactControls) do
-					local Col = GetFactionColor(faction)
-					Col.a = 20
-					
-					DrawText(faction,"DVText",Pops.x,Pops.y,Col,1) 
-				end
-				
-				for k,v in pairs(GAMEMODE.SolarSystems) do
-					if (v.SPos.visible) then
-						local Siz = 14
-						
-						if (MAP_Frame.Matches[k]) then
-							DrawOutlinedRect(v.SPos.x-Siz-4*C,v.SPos.y-Siz-4*C,Siz*2+8*C,4+Siz*2+8*C,MAIN_COLOR)
-						end
-						
-						if (Hover == k) then
-							Blu.a = 255*(cos(CurTime()*4)+1)/2
-						
-							DrawRect(v.SPos.x-2-Siz,v.SPos.y-2-Siz,4+Siz*2,4+Siz*2,Blu)
-							DrawOutlinedRect(v.SPos.x-2-Siz,v.SPos.y-2-Siz,4+Siz*2,4+Siz*2,MAIN_COLOR)
-						end
-						
-						if (v.Tech <= MAIN_SAFEZONE_TECH) then 
-							if (MAP_Frame.bShowNames) then
-								DrawText("(Safezone)","DefaultSmall",v.SPos.x,v.SPos.y-45,MAIN_GREENCOLOR,1)
-								DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,MAIN_GREENCOLOR,1)
-							end
-						else 
-							local Dat = GetSystemWar(k)
-							
-							if (Dat) then
-								local OldCol = table.Copy(MAIN_TEXTCOLOR)
-								OldCol.r = MAIN_TEXTCOLOR.r+(255-MAIN_TEXTCOLOR.r)*C
-								OldCol.g = MAIN_TEXTCOLOR.g-MAIN_TEXTCOLOR.g*C
-								OldCol.b = MAIN_TEXTCOLOR.b-MAIN_TEXTCOLOR.b*C
-								
-								local YPos = v.SPos.y-80
-								
-								DrawText(string.NiceTime(MAIN_FACTIONWAR_TIME-Dat.Ticker),"DefaultSmall",v.SPos.x,YPos,MAIN_TEXTCOLOR,1) 
-								DrawText(Dat.Attacker.." VS "..Dat.Defender,"DVTextTiny",v.SPos.x,YPos+10,OldCol,1) 
-								
-								local Prog = Dat.Points/MAIN_FACTIONWAR_BALANCEPOINT
-								
-								DrawRect(v.SPos.x-50,YPos+30,100,5,MAIN_BLACKCOLOR)
-								DrawRect(v.SPos.x-50,YPos+30,50,5,MAIN_REDCOLOR)
-								DrawRect(v.SPos.x,YPos+30,50,5,MAIN_BLUECOLOR)
-								
-								DrawOutlinedRect(v.SPos.x-50,YPos+30,100,5,MAIN_GUICOLOR)
-								
-								DrawLine(v.SPos.x-50+100*Prog,YPos+20,v.SPos.x-50+100*Prog,YPos+35,MAIN_YELLOWCOLOR)
-								DrawText(Dat.Points.."/"..(MAIN_FACTIONWAR_BALANCEPOINT-Dat.Points),"DefaultSmall",v.SPos.x,YPos+20,MAIN_TEXTCOLOR,1)
-								
-								DrawText(v.Name.." (Warzone)","DVTextTiny",v.SPos.x,v.SPos.y-30,OldCol,1)  
-							else
-								if (MAP_Frame.bShowNames) then
-									if (v.Owner) then
-										local Col = GetFactionColor(v.Owner)
-										Col.a = 250
-										
-										DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,Col,1) 
-									else
-										DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,MAIN_TEXTCOLOR,1) 
-									end
-								end
-							end
-						end
-					end
-				end 
-				
-				if (OriginSPos) then
-					local A = MAIN_TEXTCOLOR.a*1
-					
-					MAIN_TEXTCOLOR.a = 10
-					DrawText("Devinity","DVTextLarge",OriginSPos.x,OriginSPos.y,MAIN_TEXTCOLOR,1)
-					MAIN_TEXTCOLOR.a = A
-				end
-				
-				FT = UnPredictedCurTime()
-			end 
-			
-			if (lp.WarpDest and lp.WarpSPos) then
-				DrawLine(lp.SPos.x,lp.SPos.y,lp.WarpSPos.x,lp.WarpSPos.y,MAIN_BLUECOLOR)
-				DrawOutlinedRect(lp.WarpSPos.x-2,lp.WarpSPos.y-2,4,4,MAIN_BLUECOLOR)
-			end
-			
-			DrawOutlinedRect(lp.SPos.x-2,lp.SPos.y-2,4,4,MAIN_REDCOLOR)
-			
-			DrawText("You","DVTextSmall",lp.SPos.x+5,lp.SPos.y,MAIN_REDCOLOR)
-			
-			if (Hover > 0) then
-				local System = GAMEMODE.SolarSystems[Hover] 
-				
-				if (Reg != "Space" and LocalMap) then 
-					System = GAMEMODE.MapEnts[HoverEntity]
-					
-					DrawOutlinedRect(System.SPos.x-3,System.SPos.y-3,6,6,Cyan)
-					DrawText("Warp to","DVTextTiny",System.SPos.x-30,System.SPos.y-2,Cyan,1)
-					
-					if (input.MousePress(MOUSE_LEFT,"MapMouse")) then
-						LocalPlayer():SetWarpDestination(System.Pos,System.FloatPos)
-					end
-				else
-					DrawText(System.Name,"DVTextTiny",System.SPos.x,System.SPos.y-30,MAIN_TEXTCOLOR,1) 
-					DrawText("Jump to","DVTextTiny",System.SPos.x-50,System.SPos.y-2,Cyan,1)
-					
-					if (input.MousePress(MOUSE_LEFT,"MapMouse")) then
-						LocalPlayer():SetWarpDestination(System.Pos,Zero)
-					end
-				end
-			end
-			DV2P.OFF.RunFunction( "Post_MAP_Frame_Paint", s, w, h )
-		end
-		
 		local a = vgui.Create("MBButton",MAP_Frame)
 		a:SetPos(MAP_Frame:GetWide()/2+60,5)
 		a:SetSize(40,20)
@@ -498,19 +270,11 @@ function OpenMap()
 		a:SetPos(MAP_Frame:GetWide()/2-50,5)
 		a:SetSize(100,20)
 		a:SetText("Enter Local Map")
-		a.DoClick = function(s) 
-			local lp = LocalPlayer()
-			if (!lp.PlayerPos or !lp.FloatPos) then return end
-			
-			LocalMap = !LocalMap 
-			
-			if (LocalMap) then Zoom = math.Clamp(Zoom,2,70) Origin = Zero*1 s.Text = "Exit Local Map"
-			else Zoom = Zoom*10 s.Text = "Enter Local Map" end
-		end
 		a.Paint = function(s,w,h)
 			if (s.Hovered) then DrawText(s.Text,"Trebuchet18",w/2,h/2,MAIN_GREENCOLOR,1)
 			else DrawText(s.Text,"Trebuchet18",w/2,h/2,MAIN_TEXTCOLOR,1) end
 		end
+		DV2P.Map.DermaMapLocalButton = a
 		
 		local SearchPan = vgui.Create("DPanel",MAP_Frame)
 		SearchPan:SetPos(5,5)
@@ -548,6 +312,246 @@ function OpenMap()
 	end
 	
 	MAP_Frame:SetVisible(true)
+		
+	MAP_Frame.Paint = function(s,w,h)
+		DV2P.OFF.RunFunction( "Pre_MAP_Frame_Paint", s, w, h )
+		local lp = LocalPlayer()
+		
+		if (!lp.PlayerPos or !lp.FloatPos) then return end
+		
+		DrawRect(0,0,w,h,Blac)
+		
+		local Hover 		= 0
+		local HoverEntity 	= 0
+		local Reg,ID = lp:GetRegion()
+		
+		local MouseInList = input.IsMouseInBox(SysListDim.x,SysListDim.y,SysListDim.w,SysListDim.h)
+		
+		local FactControls = {}
+		
+		local OriginSPos
+		
+		cam.Start(CamData)
+			DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D", CamData )
+
+			if (LocalMap and Reg != "Space") then
+				local Scale = MAIN_SOLARSYSTEM_RADIUS/100
+				DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_LocalMap", CamData, Scale )
+				
+				for k,v in pairs(GAMEMODE.MapEnts) do v.SPos = ((v.Pos-GAMEMODE.SolarSystems[ID].Pos)/Scale+v.FloatPos/Scale):ToScreen() end
+				
+				lp.SPos = ((lp.PlayerPos-GAMEMODE.SolarSystems[ID].Pos)/Scale+lp.FloatPos/Scale):ToScreen()
+				
+				if (lp.WarpDest) then
+					lp.WarpSPos = ((lp.WarpDest-GAMEMODE.SolarSystems[ID].Pos)/Scale+lp.WarpDestDetail/Scale):ToScreen()
+				end
+				
+				RSetMaterial(Prox)
+				DrawQuadEasy(Zero,Up,200,200,MAIN_COLOR,CurTime()/3)
+
+				DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_LocalMap", CamData, Scale )
+			else
+				DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_SystemMap", CamData, Scale )
+				local Scale = MAIN_MAP_SIZE/2500
+				local Data	= lp:GetShipData()
+				local Radius = Data.MaxWarpDistance/Scale
+				local LPRealPos = lp.PlayerPos/Scale+lp.FloatPos/Scale
+				local AimFace	= -CamData.angles:Forward()
+				
+				RSetMaterial(Clouds)
+				for k,v in pairs(s.Clouds) do DrawQuadEasy(v.Pos,AimFace,v.Size,v.Size,v.Col,v.Yaw) end
+				
+				RSetMaterial(matLight)
+				
+				for k,v in pairs(GAMEMODE.SolarSystems) do 
+					DV2P.OFF.RunFunction( "Pre_OpenMap_Render3D_System", CamData, v, Scale )
+					local SunRealPos = v.Pos/Scale
+					v.SPos = SunRealPos:ToScreen() 
+					--DrawQuadEasy(SunRealPos,AimFace,50,50,v.Color,0)
+					DrawQuadEasy(SunRealPos,AimFace,5,5,v.TechColor,0)
+					
+					if (input.IsMouseInBox(v.SPos.x-14,v.SPos.y-14,28,28) and Reg!=v.Name and !MouseInList) then Hover = k end
+					
+					if (v.Owner) then
+						if (!FactControls[v.Owner]) then FactControls[v.Owner] = {} end
+						insert(FactControls[v.Owner],v.Pos)
+						
+						local Col = GetFactionColor(v.Owner)
+						Col.a = 20
+						
+						RSetMaterial(Bare)
+						render.DrawSphere( SunRealPos, 30, 32, 32, Col )
+						RSetMaterial(matLight)
+					end
+					
+					if (v.Connected) then
+						RSetMaterial(Bare)
+						for k2,CPos in pairs(v.Connected) do
+							RDrawBeam( SunRealPos, CPos/Scale, 1, 0,1,LineC )
+						end
+						RSetMaterial(matLight)
+					end
+					DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_System", CamData, v, Scale )
+				end 
+				
+				for faction,Controls in pairs(FactControls) do
+					local Sum = Zero
+					
+					for i,pos in pairs(Controls) do
+						Sum = Sum+pos
+					end
+					
+					local Avg = Sum/#Controls
+					
+					FactControls[faction] = (Avg/Scale):ToScreen()
+				end
+				
+				lp.SPos = LPRealPos:ToScreen()
+				
+				if (lp.WarpDest) then
+					lp.WarpSPos = (lp.WarpDest/Scale+lp.WarpDestDetail/Scale):ToScreen()
+				end
+				
+				RSetMaterial(Bare)
+				render.DrawSphere( LPRealPos, Radius, 32, 32, WarpCol )
+				render.DrawWireframeSphere( LPRealPos, Radius, 32, 32, WarpCol )
+				
+				OriginSPos = Zero:ToScreen()
+				DV2P.OFF.RunFunction( "Post_OpenMap_Render3D_SystemMap", CamData, Scale )
+			end
+			DV2P.OFF.RunFunction( "Post_OpenMap_Render3D", CamData )
+		cam.End()
+		
+		if (LocalMap and Reg != "Space") then
+			for k,v in pairs(GAMEMODE.MapEnts) do
+				if (table.HasValue(DisplayTable,v.Class)) then
+					DrawOutlinedRect(v.SPos.x-1,v.SPos.y-1,2,2,MAIN_WHITECOLOR)
+					DrawText(v.Class.." "..k,"DVTextSmall",v.SPos.x,v.SPos.y-20,MAIN_TEXTCOLOR,1)
+					if (input.IsMouseInBox(v.SPos.x-14,v.SPos.y-14,28,28) and (v.Pos-lp.PlayerPos-lp.FloatPos):Length() > 5000) then HoverEntity = k Hover = ID end
+				end
+			end
+			
+			DrawText(GAMEMODE.SolarSystems[ID].Name,"DVText",ScrW()/2,80,MAIN_TEXTCOLOR,1)
+		else
+			--System List
+			local FT = 0.001*(UnPredictedCurTime()-FT)
+			local C = (1+math.cos(CurTime()*3))/2
+			local Scale = MAIN_MAP_SIZE/2500
+			
+			for faction,Pops in pairs(FactControls) do
+				local Col = GetFactionColor(faction)
+				Col.a = 20
+				
+				DrawText(faction,"DVText",Pops.x,Pops.y,Col,1) 
+			end
+			
+			for k,v in pairs(GAMEMODE.SolarSystems) do
+				if (v.SPos.visible) then
+					local Siz = 14
+					
+					if (MAP_Frame.Matches[k]) then
+						DrawOutlinedRect(v.SPos.x-Siz-4*C,v.SPos.y-Siz-4*C,Siz*2+8*C,4+Siz*2+8*C,MAIN_COLOR)
+					end
+					
+					if (Hover == k) then
+						Blu.a = 255*(cos(CurTime()*4)+1)/2
+					
+						DrawRect(v.SPos.x-2-Siz,v.SPos.y-2-Siz,4+Siz*2,4+Siz*2,Blu)
+						DrawOutlinedRect(v.SPos.x-2-Siz,v.SPos.y-2-Siz,4+Siz*2,4+Siz*2,MAIN_COLOR)
+					end
+					
+					if (v.Tech <= MAIN_SAFEZONE_TECH) then 
+						if (MAP_Frame.bShowNames) then
+							DrawText("(Safezone)","DefaultSmall",v.SPos.x,v.SPos.y-45,MAIN_GREENCOLOR,1)
+							DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,MAIN_GREENCOLOR,1)
+						end
+					else 
+						local Dat = GetSystemWar(k)
+						
+						if (Dat) then
+							local OldCol = table.Copy(MAIN_TEXTCOLOR)
+							OldCol.r = MAIN_TEXTCOLOR.r+(255-MAIN_TEXTCOLOR.r)*C
+							OldCol.g = MAIN_TEXTCOLOR.g-MAIN_TEXTCOLOR.g*C
+							OldCol.b = MAIN_TEXTCOLOR.b-MAIN_TEXTCOLOR.b*C
+							
+							local YPos = v.SPos.y-80
+							
+							DrawText(string.NiceTime(MAIN_FACTIONWAR_TIME-Dat.Ticker),"DefaultSmall",v.SPos.x,YPos,MAIN_TEXTCOLOR,1) 
+							DrawText(Dat.Attacker.." VS "..Dat.Defender,"DVTextTiny",v.SPos.x,YPos+10,OldCol,1) 
+							
+							local Prog = Dat.Points/MAIN_FACTIONWAR_BALANCEPOINT
+							
+							DrawRect(v.SPos.x-50,YPos+30,100,5,MAIN_BLACKCOLOR)
+							DrawRect(v.SPos.x-50,YPos+30,50,5,MAIN_REDCOLOR)
+							DrawRect(v.SPos.x,YPos+30,50,5,MAIN_BLUECOLOR)
+							
+							DrawOutlinedRect(v.SPos.x-50,YPos+30,100,5,MAIN_GUICOLOR)
+							
+							DrawLine(v.SPos.x-50+100*Prog,YPos+20,v.SPos.x-50+100*Prog,YPos+35,MAIN_YELLOWCOLOR)
+							DrawText(Dat.Points.."/"..(MAIN_FACTIONWAR_BALANCEPOINT-Dat.Points),"DefaultSmall",v.SPos.x,YPos+20,MAIN_TEXTCOLOR,1)
+							
+							DrawText(v.Name.." (Warzone)","DVTextTiny",v.SPos.x,v.SPos.y-30,OldCol,1)  
+						else
+							if (MAP_Frame.bShowNames) then
+								if (v.Owner) then
+									local Col = GetFactionColor(v.Owner)
+									Col.a = 250
+									
+									DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,Col,1) 
+								else
+									DrawText(v.Name,"DVTextTiny",v.SPos.x,v.SPos.y-30,MAIN_TEXTCOLOR,1) 
+								end
+							end
+						end
+					end
+				end
+			end 
+			
+			if (OriginSPos) then
+				local A = MAIN_TEXTCOLOR.a*1
+				
+				MAIN_TEXTCOLOR.a = 10
+				DrawText("Devinity","DVTextLarge",OriginSPos.x,OriginSPos.y,MAIN_TEXTCOLOR,1)
+				MAIN_TEXTCOLOR.a = A
+			end
+			
+			FT = UnPredictedCurTime()
+		end 
+		
+		if (lp.WarpDest and lp.WarpSPos) then
+			DrawLine(lp.SPos.x,lp.SPos.y,lp.WarpSPos.x,lp.WarpSPos.y,MAIN_BLUECOLOR)
+			DrawOutlinedRect(lp.WarpSPos.x-2,lp.WarpSPos.y-2,4,4,MAIN_BLUECOLOR)
+		end
+		
+		DrawOutlinedRect(lp.SPos.x-2,lp.SPos.y-2,4,4,MAIN_REDCOLOR)
+		
+		DrawText("You","DVTextSmall",lp.SPos.x+5,lp.SPos.y,MAIN_REDCOLOR)
+		
+		if (Hover > 0) then
+			local System = GAMEMODE.SolarSystems[Hover] 
+			
+			if (Reg != "Space" and LocalMap) then 
+				System = GAMEMODE.MapEnts[HoverEntity]
+				
+				DrawOutlinedRect(System.SPos.x-3,System.SPos.y-3,6,6,Cyan)
+				DrawText("Warp to","DVTextTiny",System.SPos.x-30,System.SPos.y-2,Cyan,1)
+				
+				if (input.MousePress(MOUSE_LEFT,"MapMouse")) then
+					LocalPlayer():SetWarpDestination(System.Pos,System.FloatPos)
+				end
+			else
+				DrawText(System.Name,"DVTextTiny",System.SPos.x,System.SPos.y-30,MAIN_TEXTCOLOR,1) 
+				DrawText("Jump to","DVTextTiny",System.SPos.x-50,System.SPos.y-2,Cyan,1)
+				
+				if (input.MousePress(MOUSE_LEFT,"MapMouse")) then
+					LocalPlayer():SetWarpDestination(System.Pos,Zero)
+				end
+			end
+		end
+		DV2P.OFF.RunFunction( "Post_MAP_Frame_Paint", s, w, h )
+	end
+	
+	DV2P.Map.DermaMapLocalButton.DoClick = DermaMapLocalButtonDoClick
 
 	DV2P.OFF.RunFunction( "Post_OpenMap" )
 end
