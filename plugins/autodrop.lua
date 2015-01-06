2d91623a4ce4ab37db405e4e7405b8c54918055c
Metamist <metamist@ymail.com>
2015-01-06 01:23:20 +0100

    Fixed autodrop

--- plugins/autodrop.lua
+++ plugins/autodrop.lua
@@ -1,14 +1,18 @@
 PLUGIN.Name = "Auto Drop"
+PLUGIN._autodrop = PLUGIN._autodrop or -1
+PLUGIN._time = PLUGIN._time or 0
 
-local AUTODROP = -1
 concommand.Add("dv2_autodrop", function(_,_,_,str)
-	AUTODROP = tonumber(str or -1) or -1
+	DV2P.GetPlugin( "Auto Drop" )._autodrop = tonumber(str or -1) or -1
 end)
 
-hook.Add("Tick", "dropautodrop", function()
-	if AUTODROP == -1 then return end
+function PLUGIN:Think()
+	if CurTime() <= self._time then return end
+	self._time = CurTime() + 0.5
+
+	if self._autodrop == -1 then return end
 	local lp = LocalPlayer()
 	for k,v in pairs(lp:GetInventory()) do
-		if v.Data.ID == AUTODROP then lp:RequestDeleteItem(k, v.Quantity) end
+		if v.Data.ID == self._autodrop then lp:RequestDeleteItem(k, v.Quantity) end
 	end
-end)
+end
